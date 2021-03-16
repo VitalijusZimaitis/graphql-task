6177b55643e06dbab314d76634ae63b782fa9a4d
Vitalijus Zimaitis <vz@frontit.dk>
2021-03-16 12:59:15 +0200

    Initial commit adding GraphQL express server

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const mainMenu = [
+  { name: "Projects", url: "#", title: "Test" },
+  { name: "About Us", url: "#", title: "Test" },
+  { name: "Testimonials", url: "#", title: "Test" },
+  { name: "Contacts", url: "#", title: "Test" },
+];
+
+const footerMenu = [
+  { name: "Home", url: "/", title: "Test" },
+  { name: "Projects", url: "#", title: "Test" },
+  { name: "About Us", url: "#", title: "Test" },
+  { name: "Testimonials", url: "#", title: "Test" },
+  { name: "Contacts", url: "#", title: "Test" },
+];
+
+const pageContent = {
+  title: "Lorem ipsum dolor sit met, consecte",
+  content:
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.",
+  formTitle: "Lorem ipsum dolor",
+  formIntro:
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam.",
+  infoBlock: [
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam.",
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam.",
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam.",
+  ],
+  footerContent:
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.",
+};
+
+const schema = buildSchema(`
+  type NavLink {
+    name: String!
+    url: String!
+    title: String
+  }
+  type Menu {
+    mainMenu: [NavLink]
+    footerMenu: [NavLink]
+  }
+  type PageData {
+    title: String!
+    content: String!
+    formTitle: String!
+    formIntro: String!
+    infoBlock: [String]
+    footerContent: String
+  }
+  type Query {
+    pageContent: PageData
+    navigation: Menu
+  }
+`);
+
+const root = {
+  pageContent: () => pageContent,
+  navigation: () => {
+    return {
+      mainMenu: mainMenu,
+      footerMenu: footerMenu,
+    };
+  },
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000);
